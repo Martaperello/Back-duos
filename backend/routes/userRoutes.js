444e57ea5d9464b38f29e550ef4bb1c11e31458f
Marta <mperellolo@uoc.edu>
2025-01-24 18:11:30 +0100

    Restringir administrador

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const { signUp, loginUser, protect } = require('../controllers/authControllers');
+const { signUp, loginUser, protect, restrictTo } = require('../controllers/authControllers');
 const router = express.Router();
 
 router.post('/signup', signUp);
@@ -8,5 +8,9 @@ router.post('/login', loginUser)
 
 // Route to verify user authentication 
 router.get('/auth-check', protect, (req, res) => { res.status(200).json({ status: 'success', }); });
-
+router.get('/admin-check', protect, restrictTo('admin'), (req, res) => {
+    res.status(200).json({
+      status: 'success',
+    });
+  });
 module.exports = router;
