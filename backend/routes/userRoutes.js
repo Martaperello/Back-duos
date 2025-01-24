4caf33f59a58a2f30c538e48677bf9373221f087
Marta <mperellolo@uoc.edu>
2025-01-24 17:33:33 +0100

    Arreglo importacion backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const { signUp, loginUser } = require('../controllers/authControllers');
+const { signUp, loginUser, protect } = require('../controllers/authControllers');
 const router = express.Router();
 
 router.post('/signup', signUp);
